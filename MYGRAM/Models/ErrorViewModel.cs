7c42a6b91173c5d3e12f7b970ac23468903066ed
Pablo Gamez Jr <>
2021-05-15 11:41:29 -0600

    Module 1

---
+++ MYGRAM/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MYGRAM.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ MYGRAM/wwwroot/js/Index.js
@@ -0,0 +1,8 @@
+﻿$("#MainContainer").append("<br><span>My Gram</span>");
+
+$("#ButtonContainer").append("<br><span>Image 1  Image 2  Image 3 Image 3 Image 4 Image 5</span>");
+
+
+$("#FooterContainer").append("<br><span>About Blog Jobs Help</span>");
+
+$("#Footer2Container").append("<span>English 2021 MyGram</span>");
