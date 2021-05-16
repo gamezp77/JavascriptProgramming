d42414ea5631381a5f9efdf11eb9467740fbf9eb
Pablo Gamez Jr <>
2021-05-15 22:15:39 -0600

    Module 2

--- MYGRAM/wwwroot/js/Index.js
+++ MYGRAM/wwwroot/js/Index.js
@@ -6,3 +6,12 @@ $("#ButtonContainer").append("<br><span>Image 1  Image 2  Image 3 Image 3 Image 
 $("#FooterContainer").append("<br><span>About Blog Jobs Help</span>");
 
 $("#Footer2Container").append("<span>English 2021 MyGram</span>");
+
+
+$("Button").on("click", function () {
+    alert("user and password");
+});
+
+$("Button2").on("click", function () {
+    alert("you have logged in");
+});
